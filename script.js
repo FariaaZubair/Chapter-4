cb5dc1f8e9b141a18ef8cf9aec02d9a338a96ecf
Fariaa Zubair <fariaazubair27@gmail.com>
2024-08-13 01:57:53 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// var schoolName ; collegeName ; universityName;
+
+
+// -----------Declaration of legal variables--------------
+// var myName;
+// var MyFatherName;
+// var _age;
+// var user1;
+// var user_1;
+
+
+// --------------------Declaration of illegal variables------------------
+// var @gmail;
+// var 1st_user;
+// var const;
+// var my name;
+// var My-Father-Name;
+
+document.write("<h1>Rules for naming JS variables</h1>");
+var part_a = "Variable names can only contain, numbers , $ and _. For example: $my_1stVariable. </br>";
+document.write(part_a);
+
+var part_b = "Variable must begin with a letter, $ or _. For example: $name, _name or name. </br>";
+document.write(part_b);
+
+var part_c= "Variable names are case sensitive. </br>";
+document.write(part_c);
+
+var part_d= "Variable names should not be Js keywords. </br>";
+document.write(part_d);
